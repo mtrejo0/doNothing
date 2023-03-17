2bcfeb12341226a94b00dcf3aa8073cb3cca8bb3
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2023-03-17 10:47:14 -0400

    set time

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,14 @@
-import { Box } from "@mui/material";
+import { Box, Button, Menu, Slider, Typography } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
 export const Home = () => {
-  const [timer, setTimer] = useState(120); // 2 minutes in seconds
+  const [totalTime, setTotalTime] = useState(120);
+  const [timer, setTimer] = useState(totalTime); // 2 minutes in seconds
   const intervalIdRef = useRef<any>(null);
   const [message, setMessage] = useState("");
+  const [breatheInTime, setBreatheInTime] = useState(4);
+  const [holdTime, setHoldTime] = useState(7);
+  const [breatheOutTime, setBreatheOutTime] = useState(8);
 
   useEffect(() => {
     // Start the timer
@@ -14,8 +18,9 @@ export const Home = () => {
 
     // Reset the timer if the user moves their mouse or presses a key
     const resetTimer = () => {
-      setTimer(120);
+      setTimer(totalTime);
     };
+
     window.addEventListener("mousemove", resetTimer);
     window.addEventListener("keydown", resetTimer);
 
@@ -25,45 +30,122 @@ export const Home = () => {
       window.removeEventListener("mousemove", resetTimer);
       window.removeEventListener("keydown", resetTimer);
     };
-  }, []);
+  }, [totalTime]);
 
   useEffect(() => {
-    
-    if (timer % 120 === 0) {
+    const secondsElapsed = totalTime - timer;
+    const totalCycleSeconds = breatheInTime + holdTime + breatheOutTime;
+    const cycleSecondsElapsed = secondsElapsed % totalCycleSeconds;
 
-        setMessage("ok lets go")
+    let message = "";
+    if (cycleSecondsElapsed < breatheInTime) {
+      message = `Breathe in ${breatheInTime - cycleSecondsElapsed}`;
+    } else if (cycleSecondsElapsed < breatheInTime + holdTime) {
+      message = `Hold ${breatheInTime + holdTime - cycleSecondsElapsed}`;
+    } else {
+      message = `Breathe out ${totalCycleSeconds - cycleSecondsElapsed}`;
     }
-    else if (timer % 6 === 0) {
-
-        setMessage("6")
-    }
-    else if (timer % 7 === 0) {
-
-        setMessage("7")
-    }
-    else if (timer % 8 === 0) {
-
-        setMessage("8")
-    }
-    else if (timer % 9 === 0) {
-
-        setMessage("9")
-    }
-    else if (timer % 10 === 0) {
-
-        setMessage("10")
-    }
+    setMessage(message);
   }, [timer]);
-
 
   // Format the timer value as MM:SS
   const formattedTime = new Date(timer * 1000).toISOString().substr(14, 5);
 
-  return <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh"}}>
+  const [anchorEl, setAnchorEl] = useState(null);
 
-    <p>Do nothing for 2 min</p>
-    <p>{formattedTime}</p>
-    <p>{message}</p>
-    
+  const handleOpen = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        position: "relative",
+      }}
+    >
+      <img
+        src={process.env.PUBLIC_URL + "/images/sunset-shimmer-sunsets.gif"}
+        alt="background"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+          zIndex: "1",
+        }}
+      >
+        <Typography>Do nothing for 2 min</Typography>
+        <br></br>
+        <Typography>{formattedTime}</Typography>
+        <br></br>
+        <Typography>{message}</Typography>
+        <br></br>
+
+        <Button onClick={handleOpen}>Settings</Button>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <Box sx={{ margin: "32px" }}>
+            <Typography>Total Time: {totalTime} seconds</Typography>
+            <Slider
+              value={totalTime}
+              onChange={(event, newValue) => setTotalTime(newValue as number)}
+              min={30}
+              max={600}
+              step={1}
+            />
+            <Typography>Breathe In Time: {breatheInTime} seconds</Typography>
+            <Slider
+              value={breatheInTime}
+              onChange={(event, newValue) =>
+                setBreatheInTime(newValue as number)
+              }
+              min={1}
+              max={10}
+              step={1}
+            />
+            <Typography>Hold Time: {holdTime} seconds</Typography>
+            <Slider
+              value={holdTime}
+              onChange={(event, newValue) => setHoldTime(newValue as number)}
+              min={1}
+              max={10}
+              step={1}
+            />
+            <Typography>Breathe Out Time: {breatheOutTime} seconds</Typography>
+            <Slider
+              value={breatheOutTime}
+              onChange={(event, newValue) =>
+                setBreatheOutTime(newValue as number)
+              }
+              min={1}
+              max={10}
+              step={1}
+            />
+          </Box>
+        </Menu>
+      </Box>
     </Box>
+  );
 };
